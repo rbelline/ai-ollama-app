1b96a2c6f44928f761f5698544956186929879c3
rbellinello <bellinelloriccardo@gmail.com>
2024-11-29 13:08:11 +0100

    finish embedded pdf ai reader

---
+++ pdf-rag-clean.py
@@ -0,0 +1,22 @@
+# main.py
+
+import os
+import logging
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+import ollama
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Constants
+
+def ingest_pdf(doc_path):
+    logging.info()
--- pdf-rag.py
+++ pdf-rag.py
@@ -8,4 +8,94 @@
 ## run pip install -r requirements.txt to install the required packages
 
 from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_community.document_loaders import OnlinePDFLoader
+
+doc_path ="./data/BOI.pdf"
+model = "llama3.2"
+
+#load pdf file
+if doc_path:
+    loader = UnstructuredPDFLoader(file_path=doc_path)
+    data=loader.load()
+    print("done loading...")
+else:
+    print("Updload a pdf file")
+
+content = data[0].page_content
+print(content[0:100])
+
+# ==== End of PDF Ingestion ====
+
+# === Extract text from PDF FIles ans split into small chunks ===
+
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=300)
+chunks = text_splitter.split_documents(data)
+print("done splitting...")
+
+# print(f"Number of chunks: {len(chunks)}")
+# print(f"Example chunks: {chunks[0]}")
+
+# === Add to vector database ===
+
+import ollama
+ollama.pull("nomic-embed-text")
+
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embedding=OllamaEmbeddings(model="nomic-embed-text"),
+    collection_name="simple-rag"
+)
+print("done adding to vector database")
+
+# === Retrieval ===
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_ollama import ChatOllama
+
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+llm = ChatOllama(model=model)
+
+# a simple technique to generate multiple questions from a single question and then retrieve documents
+# based on those questions, getting the best of both worlds.
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is to generate five
+    different versions of the given user question to retrieve relevant documents from
+    a vector database. By generating multiple perspectives on the user question, your
+    goal is to help the user overcome some of the limitations of the distance-based
+    similarity search. Provide these alternative questions separated by newlines.
+    Original question: {question}""",
+)
+
+retriever = MultiQueryRetriever.from_llm(
+    vector_db.as_retriever(), llm, prompt=QUERY_PROMPT
+)
+
+# Rag prompt
+template = """Answer the question based ONLY on the following context:
+{context}
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+res = chain.invoke(input=("what is the document about?",))
+# res = chain.invoke(
+#     input=("what are the main points as a business owner I should be aware of?",)
+# )
+# res = chain.invoke(input=("how to report BOI?",))
+
+print(res)
